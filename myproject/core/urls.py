587836aff3308d7955498d368aef445928fe6190
rg3915 <rg3915@yahoo.com.br>
2019-05-15 03:52:30 -0300

    Demais páginas.

--- myproject/core/urls.py
+++ myproject/core/urls.py
@@ -7,4 +7,22 @@ app_name = 'core'
 
 urlpatterns = [
     path('', v.index, name='index'),
+    path('blank/', v.blank, name='blank'),
+    path('dashboard/', v.dashboard, name='dashboard'),
+    path('email/', v.email, name='email'),
+    path('compose/', v.compose, name='compose'),
+    path('calendar/', v.calendar, name='calendar'),
+    path('chat/', v.chat, name='chat'),
+    path('charts/', v.charts, name='charts'),
+    path('forms/', v.forms, name='forms'),
+    path('ui/', v.ui, name='ui'),
+    path('basic_table/', v.basic_table, name='basic_table'),
+    path('datatable/', v.datatable, name='datatable'),
+    path('google_maps/', v.google_maps, name='google_maps'),
+    path('vector_maps/', v.vector_maps, name='vector_maps'),
+    path('p404/', v.p404, name='p404'),
+    path('p500/', v.p500, name='p500'),
+    path('signin/', v.signin, name='signin'),
+    path('signup/', v.signup, name='signup'),
+    path('buttons/', v.buttons, name='buttons'),
 ]
--- myproject/core/views.py
+++ myproject/core/views.py
@@ -4,3 +4,93 @@ from django.shortcuts import render
 def index(request):
     template_name = 'index.html'
     return render(request, template_name)
+
+
+def blank(request):
+    template_name = 'blank.html'
+    return render(request, template_name)
+
+
+def dashboard(request):
+    template_name = 'dashboard.html'
+    return render(request, template_name)
+
+
+def email(request):
+    template_name = 'email.html'
+    return render(request, template_name)
+
+
+def compose(request):
+    template_name = 'compose.html'
+    return render(request, template_name)
+
+
+def calendar(request):
+    template_name = 'calendar.html'
+    return render(request, template_name)
+
+
+def chat(request):
+    template_name = 'chat.html'
+    return render(request, template_name)
+
+
+def charts(request):
+    template_name = 'charts.html'
+    return render(request, template_name)
+
+
+def forms(request):
+    template_name = 'forms.html'
+    return render(request, template_name)
+
+
+def ui(request):
+    template_name = 'ui.html'
+    return render(request, template_name)
+
+
+def basic_table(request):
+    template_name = 'basic-table.html'
+    return render(request, template_name)
+
+
+def datatable(request):
+    template_name = 'datatable.html'
+    return render(request, template_name)
+
+
+def google_maps(request):
+    template_name = 'google-maps.html'
+    return render(request, template_name)
+
+
+def vector_maps(request):
+    template_name = 'vector-maps.html'
+    return render(request, template_name)
+
+
+def p404(request):
+    template_name = '404.html'
+    return render(request, template_name)
+
+
+def p500(request):
+    template_name = '500.html'
+    return render(request, template_name)
+
+
+def signin(request):
+    template_name = 'signin.html'
+    return render(request, template_name)
+
+
+def signup(request):
+    template_name = 'signup.html'
+    return render(request, template_name)
+
+
+def buttons(request):
+    template_name = 'buttons.html'
+    return render(request, template_name)
